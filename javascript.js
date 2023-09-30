c6f0cb23691470413c656dcde31cb98ea17c4d13
Bhargav Pattanayak <122164885+Bhargav00007@users.noreply.github.com>
2023-09-30 10:49:05 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let go = document.getElementById('go');
+
+var a = 100;
+var b = 133;
+
+function val(){
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+let input3 = document.getElementById('input3');
+let output = document.getElementById('output');
+
+let day1 = (input1.value/a)*input2.value;
+let day2 = (input3.value/a)*b;
+
+let goal = day2 - day1;
+
+output.innerHTML = `You have to go more ${goal} days to reach your goal!`;
+
+};
+
